d7219e005ffb3a3824a98f2688f2f0a71de7ba03
Bealby <mgbealby@gmail.com>
2023-02-11 14:34:10 +0100

    Set-up about/ portfolio pps. Set up functionalityslity

--- murray_bealby/urls.py
+++ murray_bealby/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('about', include('about.urls')),
+    path('portfolio', include('portfolio.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ portfolio/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def portfolio(request):
+    # A view to return the portfolio page
+    return render(request, "portfolio/portfolio.html")
