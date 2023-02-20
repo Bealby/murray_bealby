8babe6537b9a116c88edc08404ede8ecc5e801e1
Bealby <mgbealby@gmail.com>
2023-02-20 17:54:21 +0100

    Add image field to model

--- portfolio/models.py
+++ portfolio/models.py
@@ -9,6 +9,7 @@ class Project(models.Model):
     website = models.CharField(max_length=254)
     image_front = models.ImageField(null=True, blank=True)
     image_back = models.ImageField(null=True, blank=True)
+    image_mockup = models.ImageField(null=True, blank=True)
     code = models.TextField(max_length=1000, null=True, blank=True)
     description = models.TextField(max_length=1000, null=True, blank=True)
     position = models.CharField(max_length=254, null=True, blank=True)
