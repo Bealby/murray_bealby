018b4580a87764a9f0dee7d4eb88f82569ec671e
Bealby <mgbealby@gmail.com>
2023-02-22 18:27:04 +0100

    Remove moday from about

--- about/models.py
+++ about/models.py
@@ -1,18 +1,1 @@
 from django.db import models
-'''Pillow must be installed'''
-
-
-class Project(models.Model):
-    '''Programmatic Name'''
-    name = models.CharField(max_length=254)
-    github = models.CharField(max_length=254)
-    website = models.CharField(max_length=254)
-    image_front = models.ImageField(null=True, blank=True)
-    image_back = models.ImageField(null=True, blank=True)
-    image_mockup = models.ImageField(null=True, blank=True)
-    code = models.TextField(max_length=1000, null=True, blank=True)
-    description = models.TextField(max_length=1000, null=True, blank=True)
-    position = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
