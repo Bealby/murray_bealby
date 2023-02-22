e7e8b085b0a84d61630d2315df6695872fd655c4
Bealby <mgbealby@gmail.com>
2023-02-22 15:38:38 +0100

    Deployment settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn murray_bealby.wsgi:application
--- murray_bealby/settings.py
+++ murray_bealby/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import dj_database_url
+if os.path.exists('env.py'):
+    import env
 
 from pathlib import Path
 
@@ -22,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gcxqy)qi%btm(eukhe6$x8!9p!-69ui(oda9@u^7m3fj)16csk'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['murray-bealby.herokuapp.com', 'localhost', '127.0.0.1',
+                 'www.murray-bealby.com', 'murray-bealby.com',
+                 'www.murray-bealby.org', 'murray-bealby.org']
 
 
 # Application definition
@@ -82,12 +86,17 @@ WSGI_APPLICATION = 'murray_bealby.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
