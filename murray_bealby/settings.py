697f43b2d8d3d6263899119ee21417782ada2ea6
Bealby <mgbealby@gmail.com>
2023-02-25 20:30:11 +0100

    Added domains to allowed hosts

--- murray_bealby/settings.py
+++ murray_bealby/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['murray-bealby.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['murray-bealby.herokuapp.com', 'localhost', '127.0.0.1',
+                'www.murraybealby.com', 'murraybealby.com']
 
 # Application definition
 
