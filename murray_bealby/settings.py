f7002b9cda127b1e101346fddc8ac3fb3fc7cd82
Bealby <mgbealby@gmail.com>
2025-07-05 06:14:12 +0200

    Fix S3 storage config

--- murray_bealby/settings.py
+++ murray_bealby/settings.py
@@ -163,7 +163,7 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-# Default primary key field type
+# Default primary key field type 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
