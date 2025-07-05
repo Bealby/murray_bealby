c09729349f6fe1da5b2887ae5dd32872adf14412
Bealby <mgbealby@gmail.com>
2025-07-05 06:40:31 +0200

    Test

--- custom_storages.py
+++ custom_storages.py
@@ -9,16 +9,3 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
-    default_acl = 'public-read'  # Make static files publicly accessible
-
-
-class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
-    default_acl = 'public-read'  # You can set to 'private' if you prefer
-    file_overwrite = False  # Prevent overwriting files with the same name
